28da42ae0023175684cf7b24489b0a3d58f8ccbd
Turadg Aleahmad <turadg@agoric.com>
2024-01-26 13:00:35 -0800

    chore: assert PROPOSAL_PATH param

--- packages/synthetic-chain/upgrade-test-scripts/install_deps.sh
+++ packages/synthetic-chain/upgrade-test-scripts/install_deps.sh
@@ -4,6 +4,11 @@ set -e
 
 PROPOSAL_PATH=$1
 
+if [ -z "$PROPOSAL_PATH" ]; then
+    echo "Must specify what proposal to install"
+    exit 1
+fi
+
 # The base image is Node 16.9, which supports Corepack.
 # Yarn v4 requires Node 18+ but so far it's working with 16.19.
 export YARN_IGNORE_NODE=1
@@ -14,12 +19,10 @@ corepack enable
 cd "$(dirname "$(realpath -- "$0")")"
 
 # TODO consider yarn workspaces to install all in one command
-if [ -n "$PROPOSAL_PATH" ]; then
-    cd "../proposals/$PROPOSAL_PATH"
+cd "../proposals/$PROPOSAL_PATH"
 
-    if test -f "yarn.lock"; then
-        yarn --version # only Berry supported, so next commands will fail on classic
-        yarn config set --home enableTelemetry 0
-        yarn install --immutable
-    fi
+if test -f "yarn.lock"; then
+    yarn --version # only Berry supported, so next commands will fail on classic
+    yarn config set --home enableTelemetry 0
+    yarn install --immutable
 fi
