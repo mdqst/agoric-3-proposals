203b00e676b3527da8d08ec314437dcd239ebae4
Turadg Aleahmad <turadg@agoric.com>
2024-03-08 11:19:25 -0800

    fixup! fix: gas=auto for all submit-proposal

--- proposals/61:lo-bundle-cost/eval.sh
+++ proposals/61:lo-bundle-cost/eval.sh
@@ -5,7 +5,8 @@ set -e
 
 source /usr/src/upgrade-test-scripts/env_setup.sh
 
+# shellcheck disable=SC2086
 agd tx gov submit-proposal param-change lower-bundle-cost.json \
-	${SUBMIT_PROPOSAL_OPTS:+"--missing-env-setup"}
+	${SUBMIT_PROPOSAL_OPTS="--missing-env-setup"}
 
 voteLatestProposalAndWait
