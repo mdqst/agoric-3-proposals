3115c2c95645ff9c928f853f1af17ae6e18f2a5b
Turadg Aleahmad <turadg@agoric.com>
2024-03-08 10:42:33 -0800

    fixup! Test emerynet sequential rc0 and rc1 upgrades

--- proposals/b:upgrade-14-rc1/test.sh
+++ proposals/b:upgrade-14-rc1/test.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# Place here any test that should be executed using the executed proposal.
-# The effects of this step are not persisted in further proposal layers.
-
 echo "No post tests of upgrade-14-rc1. Build success is sufficient."
